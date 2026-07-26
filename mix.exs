00fd28564be980de84570bb840037beef8f1fbaa
Alisson Machado <aamachado@inf.ufsm.br>
2026-07-25 21:27:05 -0300

    tag: 1.1.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Mnemo.MixProject do
   def project do
     [
       app: :mnemo,
-      version: "1.0.1",
+      version: "1.1.0",
       elixir: "~> 1.15",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
